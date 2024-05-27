14dc9eef5e7fa1254ade1151c98f0f224f5bfa9e
itsjohncs <johnsullivan.pem@gmail.com>
2024-05-27 12:43:21 -0700

    Remove the logs.

--- packages/web-frontend/src/app/roll/index.ts
+++ packages/web-frontend/src/app/roll/index.ts
@@ -3,10 +3,8 @@ import { DiceRoll } from "@dice-roller/rpg-dice-roller";
 export default function roll(input: string): string {
     try {
         const diceRoll = new DiceRoll(input);
-        console.log(diceRoll);
         return `${diceRoll.rolls} = ${diceRoll.total}`;
     } catch (e) {
-        console.log(e);
         return e + "";
     }
 }
