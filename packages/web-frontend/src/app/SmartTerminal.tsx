fdce82f9e051da3d27b66d30ce38c34ccaf02c7d
itsjohncs <johnsullivan.pem@gmail.com>
2024-05-27 12:32:51 -0700

    Place prompt into the log as well.

--- packages/web-frontend/src/app/SmartTerminal.tsx
+++ packages/web-frontend/src/app/SmartTerminal.tsx
@@ -4,20 +4,22 @@ import {useCallback, useState} from "react";
 import Terminal from "./Terminal";
 import roll from "./roll";
 
+const prompt = "$ ";
+
 export default function SmartTerminal() {
     const [lines, setLines] = useState<string[]>([]);
 
     const handleSubmit = useCallback(
         function (value: string) {
             setLines(function (prev) {
-                return [...prev, roll(value)];
+                return [...prev, `${prompt}${value}`, roll(value)];
             });
         },
         [setLines],
     );
 
     return (
-        <Terminal prompt="$ " onSubmit={handleSubmit}>
+        <Terminal prompt={prompt} onSubmit={handleSubmit}>
             {lines.map((i, idx) => (
                 <div key={idx}>{i}</div>
             ))}
